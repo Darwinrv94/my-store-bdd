527f7b839af0f531fec20e5b26c6c4a37673cc12
Darwin Rodriguez <darwinrv94@gmail.com>
2022-07-31 09:21:42 -0400

    Configuración y ejecución de migraciones

--- libs/sequealize.js
+++ libs/sequealize.js
@@ -4,16 +4,16 @@ const setupModels = require('../db/models/index');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-//const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+//const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'mysql',//'postgres',
+  dialect: 'postgres',//'mysql',
   logging: true
 });
 
 setupModels(sequelize);
 
-sequelize.sync();
+// sequelize.sync();
 
 module.exports = sequelize;
